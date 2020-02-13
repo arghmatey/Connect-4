52de124bd42eb5c186a052e66e8573fec3880adb
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-02-13 15:58:26 -0800

    Add win message

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 let board = [];
-let turn;
-let winner;
+let turn, winner, score;
 
 const playerChoices = document.querySelector('#player-choice').querySelectorAll('div');
 const gameboardColumns = document.querySelectorAll('column');
 const gameBoard = document.querySelector('#board');
-const newGame = document.querySelector('#new-game');
+const newBoard = document.querySelector('#new-board');
+const messageBox = document.querySelector('#winner')
 const pOneEl = document.querySelector('#player1');
 const pTwoEl = document.querySelector('#player2');
+const pOneScore = document.querySelector('#player1 div')
+const pTwoScore = document.querySelector('#player2 div')
 
 const playerInfo = {
     '0': {
@@ -15,11 +17,13 @@ const playerInfo = {
         'id': ''
     },
     '1': {
+        'name': 'Player One',
         'color': 'var(--purple)',
         'id': 'player-one-color',
         'scoreBox': pOneEl
     },
     '-1': {
+        'name': 'Player Two',
         'color': 'var(--mustard)',
         'id': 'player-two-color',
         'scoreBox': pTwoEl
@@ -28,7 +32,7 @@ const playerInfo = {
 
 playerChoices.forEach(function (choice) {
     choice.addEventListener('mouseover', function (evt) {
-        event.target.style.backgroundColor = playerInfo[turn]['color'];
+        event.target.style.backgroundColor = playerInfo[turn].color;
     }),
         choice.addEventListener('mouseout', function (evt) {
             event.target.style.backgroundColor = 'transparent';
@@ -36,69 +40,78 @@ playerChoices.forEach(function (choice) {
         choice.addEventListener('click', clickHandler)
 });
 
-newGame.addEventListener('click', init);
+newBoard.addEventListener('click', emptyBoard);
+
+
 
 init();
 
 function winLogic() {
-    for (let v = 0; v < board.length; v++) {
-        for (let h = 0; h < board[v].length; h++) {
-            if (board[v][h] === turn && board[v][h + 1] === turn && board[v][h + 2] === turn && board[v][h + 3] === turn) {
+    for (let c = 0; c < board.length; c++) {
+        for (let r = 0; r < board[c].length; r++) {
+            if (board[c][r] === turn && board[c][r + 1] === turn && board[c][r + 2] === turn && board[c][r + 3] === turn) {
                 winner = turn;
                 renderGameStatus();
-            } else if (board[v + 1] && board[v + 2] && board[v + 3]) {
-                if (board[v][h] === turn && board[v + 1][h] === turn && board[v + 2][h] === turn && board[v + 3][h] === turn) {
+            } else if (board[c + 1] && board[c + 2] && board[c + 3]) {
+                if (board[c][r] === turn && board[c + 1][r] === turn && board[c + 2][r] === turn && board[c + 3][r] === turn) {
                     winner = turn;
                     renderGameStatus();
                 }
-                if (board[v][h] === turn && board[v + 1][h + 1] === turn && board[v + 2][h + 2] === turn && board[v + 3][h + 3] === turn) {
+                if (board[c][r] === turn && board[c + 1][r + 1] === turn && board[c + 2][r + 2] === turn && board[c + 3][r + 3] === turn) {
                     winner = turn;
                     renderGameStatus();
                 }
-                if (board[v][h] === turn && board[v + 1][h - 1] === turn && board[v + 2][h - 2] === turn && board[v + 3][h - 3] === turn) {
+                if (board[c][r] === turn && board[c + 1][r - 1] === turn && board[c + 2][r - 2] === turn && board[c + 3][r - 3] === turn) {
                     winner = turn;
                     renderGameStatus();
                 }
-            };
+            } 
         };
     };
 }
 
+
 function clickHandler(evt) {
+    if (winner) return;
     let columnNum = parseInt(event.target.getAttribute('class'));
     let slotNum = board[columnNum].lastIndexOf(0);
     if (slotNum === -1) return;
     board[columnNum][slotNum] = turn;
-    console.log(board[columnNum][slotNum - 1]);
-
     winLogic();
-
     turn *= -1;
-    event.target.style.backgroundColor = playerInfo[turn]['color'];
+    event.target.style.backgroundColor = playerInfo[turn].color;
     render();
+}
 
+function winningMessage() {
+    messageBox.style.color = `${playerInfo[winner].color}`;
+    messageBox.textContent = `${playerInfo[winner].name} wins!`
+}
+
+function renderGameStatus() {
+    if (winner === null) {
+        playerInfo[turn]['scoreBox'].setAttribute('id', playerInfo[turn].id);
+        playerInfo[turn * -1]['scoreBox'].setAttribute('id', playerInfo['0'].id);
+        messageBox.textContent = '';
+    } else if (winner === turn) {
+        score[winner]++;
+        winningMessage();
+    };
 }
 
 function render() {
+    pOneScore.textContent = score['1'];
+    pTwoScore.textContent = score['-1'];
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            let holeColor = board[i][j];
-            gameBoard.children[i].children[j].style.backgroundColor = playerInfo[holeColor]['color'];
+            let playerSelect = board[i][j];
+            gameBoard.children[i].children[j].style.backgroundColor = playerInfo[playerSelect].color;
         };
     };
     renderGameStatus();
 }
 
-function renderGameStatus() {
-    if (winner === null) {
-        playerInfo[turn]['scoreBox'].setAttribute('id', playerInfo[turn]['id']);
-        playerInfo[turn * -1]['scoreBox'].setAttribute('id', playerInfo['0']['id']);
-    } else if (winner === turn) {
-        console.log('fuck yeah winner!!!');
-    };
-}
-
-function init() {
+function emptyBoard() {
     board = [
         [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0],
@@ -111,4 +124,12 @@ function init() {
     turn = 1;
     winner = null;
     render();
+}
+
+function init() {
+    score = {
+        '1': 0,
+        '-1': 0
+    }
+    emptyBoard();
 }
